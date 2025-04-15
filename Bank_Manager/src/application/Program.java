f1ad2d4d3d63830aee2c550d852bc8ea91002cbb
Moucalou <moucalou@gmail.com>
2025-04-14 21:22:47 -0300

    Created switch case for the first option

--- Bank_Manager/src/application/Program.java
+++ Bank_Manager/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
 
@@ -10,6 +11,9 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		
 		List<String> history = new ArrayList<>();
+		List<String> owner = new ArrayList<>();
+		List<Integer> number = new ArrayList<>();
+		List<Double> balance = new ArrayList<>();
 
 		char conti = 'y';
 		
@@ -25,6 +29,46 @@ public class Program {
 			System.out.println("7 - Exit");
 			System.out.print("Please, select a number: ");
 			int num = sc.nextInt();
+			sc.nextLine();
+			
+			switch (num) {
+			case 1:
+				System.out.print("Enter account owner name: ");
+				String name = sc.nextLine();
+				owner.add(name);
+				System.out.print("Enter account number: ");
+				int accountNumber = sc.nextInt();
+				sc.nextLine();
+				number.add(accountNumber);
+				System.out.print("Do you want to start a balance? (y/n) ");
+				char startBalance = sc.next().charAt(0);
+				switch (startBalance) {
+				case 'y':
+					System.out.print("Please insert the amount of money: ");
+					double money = sc.nextDouble();
+					balance.add(money);
+					break;
+				case 'n':
+					money = 0.0;
+					balance.add(money);
+					break;
+				}
+				history.add("Created account " + number.getLast() + ". Owner " + owner.getLast() + ". Balance: R$ " + balance.getLast());
+				System.out.println("Created account " + number.getLast() + ". Owner " + owner.getLast() + ". Balance: R$ " + balance.getLast());
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			}
 			
 		}
 	}
